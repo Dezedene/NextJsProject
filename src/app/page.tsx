0138eccdcf3bd2daa0fde5474f51bfc01d4ac634
Dezedene <gregoire.suray@gmail.com>
2023-11-21 17:35:55 +0100

    added navbar

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import NavBar from './components/NavBar';
+
+export default function Home() {
+  return (
+    // <Menu />
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <NavBar />
+    </main>
+  )
+}
